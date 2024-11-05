71b5e16eacb903cb76e28418285da057a10d9631
Tomas Ribes <tomasribesb@gmail.com>
2024-11-05 16:38:00 -0300

    configuraciones basicas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
+import * as dotenv from 'dotenv';
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: 'prod.env' });
+} else {
+  dotenv.config({ path: 'dev.env' });
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('/api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PATCH,DELETE',
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000; // Utiliza el puerto definido en la variable de entorno PORT o el puerto 3000 por defecto
+  await app.listen(port);
+
+  const server = app.getHttpServer();
+  const addressInfo = server.address();
+
+  if (typeof addressInfo === 'string') {
+    console.log(`Application is listening on ${addressInfo}`);
+  } else {
+    console.log(`Application is listening on http://${addressInfo.address}:${addressInfo.port}`);
+  }
 }
 bootstrap();
